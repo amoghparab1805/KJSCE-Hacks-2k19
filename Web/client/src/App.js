ff9821a465ea792c99811a96142334915407dd0b
Amogh Parab <amoghparab247@gmail.com>
2019-09-27 01:52:24 +0530

    Phone Number Authentication

--- Web/client/src/App.js
+++ Web/client/src/App.js
@@ -1,60 +1,56 @@
 import React, { Component } from "react"
-import "./App.css"
 import firebase from "firebase"
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
 
 firebase.initializeApp({
-  apiKey: "AIzaSyDUP81oZRkZJS_TRYDnXelp9_TWNCj2OWA",
-  authDomain: "kjscehack-fa5ae.firebaseapp.com",
-  databaseURL: "https://kjscehack-fa5ae.firebaseio.com",
-  projectId: "kjscehack-fa5ae",
-  storageBucket: "",
-  messagingSenderId: "803546715173",
-  appId: "1:803546715173:web:ec618fcdabd348a278dbcc",
-  measurementId: "G-H4SLLNZ59N"
+  apiKey: "AIzaSyA8JLEUluEW6leoEAHbFiRT1LFOVs6BsKI",
+  authDomain: "kjsce-hacks-2k19.firebaseapp.com",
+  databaseURL: "https://kjsce-hacks-2k19.firebaseio.com",
+  projectId: "kjsce-hacks-2k19",
+  storageBucket: "kjsce-hacks-2k19.appspot.com",
+  messagingSenderId: "94913900845",
+  appId: "1:94913900845:web:d29d6f30c740312d3de3a4"
 })
 
 class App extends Component {
-  state = { isSignedIn: false }
+  state = { 
+    isSignedIn: false,
+    phoneNumber: '+91' 
+  }
   uiConfig = {
     signInFlow: "popup",
     signInOptions: [
       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-      firebase.auth.GithubAuthProvider.PROVIDER_ID,
-      firebase.auth.EmailAuthProvider.PROVIDER_ID
+      // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+      // firebase.auth.GithubAuthProvider.PROVIDER_ID,
+      // firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      firebase.auth.PhoneAuthProvider.PROVIDER_ID,
     ],
     callbacks: {
       signInSuccess: () => false
     }
   }
 
-  onClickPhone() {
-    const phoneNumber = this.phone;
-    const appVerifier = window.recaptchaVerifier;
-    firebase
-    .auth()
-    .signInWithPhoneNumber(phoneNumber, appVerifier)
-    .then(confirmResult => {
-      // success
-    })
-    .catch(error => {
-      // error
+  onClickPhone = () => {
+    var appVerifier = new firebase.auth.RecaptchaVerifier("recaptcha-container",
+    {
+       size:"invisible"
     });
+    var provider =firebase.auth.PhoneAuthProvider()
+    provider.verifyPhoneNumber(this.state.phoneNumber, appVerifier).then((verificationId) => {
+      var verificationCode = window.prompt('Enter the verification code.')
+      return firebase.auth.PhoneAuthProvider.credential(verificationId, verificationCode)
+    }).then((phoneCred) => {
+      return firebase.auth.signInWithCredentials(phoneCred)
+    })
   }
 
   componentDidMount = () => {
     firebase.auth().onAuthStateChanged(user => {
       this.setState({ isSignedIn: !!user })
       console.log("user", user)
-      console.log(user.getIdToken())
     })
-    window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier("recaptcha-container",
-    {
-       size:"invisible"
-        // other options
-    });
   }
 
   render() {
@@ -64,9 +60,9 @@ class App extends Component {
           <span>
             <div>Signed In!</div>
             <button onClick={() => firebase.auth().signOut()}>Sign out!</button>
-            <h1>Welcome {firebase.auth().currentUser.displayName} </h1>
+            <h1>Welcome {firebase.auth().currentUser.displayName} {firebase.auth().currentUser.phoneNumber}</h1>
             <img
-              alt="profile picture"
+              alt="profile"
               src={firebase.auth().currentUser.photoURL}
             />
           </span>
@@ -76,7 +72,6 @@ class App extends Component {
               uiConfig={this.uiConfig}
               firebaseAuth={firebase.auth()}
             />
-            <input id="recaptcha-container" type="button" onClick={this.onClickPhone} />
           </div>
         )}
       </div>
