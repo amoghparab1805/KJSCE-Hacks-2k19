c1aa83d33439cd5545c6860a9e0b4b894683d175
Amogh Parab <amoghparab247@gmail.com>
2019-10-06 12:34:05 +0530

    Final Commit

--- Web/client/src/components/SignUp.js
+++ Web/client/src/components/SignUp.js
@@ -45,9 +45,10 @@ class SignUp extends Component {
           { headers: {"Content-Type": "application/json"} }
         ).then(response => {
           console.log(response.data)
-          localStorage.setItem("token", response.data.token)
-          localStorage.setItem("isNewUser", response.data.is_new_user)
-          localStorage.setItem("uid", response.data.user.user)
+          
+          // localStorage.setItem("token", response.data.token)
+          // localStorage.setItem("isNewUser", response.data.is_new_user)
+          // localStorage.setItem("uid", response.data.user.user)
         })
       },
     }
@@ -58,6 +59,11 @@ class SignUp extends Component {
       showModal: false
     });
   };
+
+  signOutNow = () => {
+    localStorage.removeItem('token');
+    this.setState({isSignedIn: false});
+  }
 
   signOut = async() => {
     await axios.post("https://red-hat-pirates.herokuapp.com/api/auth/token/logout/", {},
@@ -94,7 +100,7 @@ class SignUp extends Component {
           ).then(response => {
             console.log(response.data)
             localStorage.setItem("token", response.data.token)
-            localStorage.removeItem("displayName")
+            // localStorage.removeItem("displayName")
           })
         }
       }
@@ -105,70 +111,73 @@ class SignUp extends Component {
     return (
     <div className="App">
         {this.state.isSignedIn ? (
-          [
-            (localStorage.getItem('isNewUser') ?
-              (
-                <WrappedDetailsForm/>
-              ): (
-                  <Layout style={{ minHeight: '100vh' }}>
-                  <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
-                    <div className="logo" />
-                    <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-                      {this.state.collapsed ? (
-                        <Menu.Item key="11" disabled style={{height:'50px'}}>
-                          <img
-                            alt="profile"
-                            style={{marginTop:'10px',marginLeft:'-12px', width:'40px', height: '40px'}}
-                            src={firebase.auth().currentUser.photoURL}
-                          />
-                        </Menu.Item>
-                      ) : (
-                        <Menu.Item key="9" disabled style={{height:'75px'}}>
-                          <img
-                              alt="profile"
-                              style={{marginTop:'10px', width:'60px', height: '60px'}}
-                              src={firebase.auth().currentUser.photoURL}
-                            />&nbsp;&nbsp;
-                          <span style={{fontSize:'14px', color:'white', fontWeight:'bold'}}>{firebase.auth().currentUser.displayName}</span>
-                        </Menu.Item>
-                      )}
-                      <Menu.Item key="1" style={{height:'60px', fontSize:'18px'}}>
-                        <Icon type="stock" />
-                        <span>Stocks</span>
-                      </Menu.Item>
-                      <Menu.Item key="5" style={{height:'60px'}}>
-                        <Icon type="pie-chart" />
-                        <span>Investments</span>
-                      </Menu.Item>
-                      <Menu.Item key="2" style={{height:'60px'}}>
-                        <Icon type="desktop" />
-                        <span>Customer Care</span>
-                      </Menu.Item>
-                      <Menu.Item key="3" style={{height:'60px'}}>
-                        <Icon type="user" />
-                        <span>User</span>
-                      </Menu.Item>
-                      <Menu.Item key="4" onClick={this.signOut} style={{height:'60px'}}>
-                        <Icon type="file" />
-                        <span>Logout</span>
-                      </Menu.Item>
-                    </Menu>
-                  </Sider>
-                  <Layout>
-                    <Header style={{ background: '#fff', padding: 0 }} />
-                    <Content style={{ margin: '0 16px' }}>
-                      <Breadcrumb style={{ margin: '16px 0' }}>
-                        <Breadcrumb.Item>User</Breadcrumb.Item>
-                        <Breadcrumb.Item>Bill</Breadcrumb.Item>
-                      </Breadcrumb>
-                      <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>Bill is a cat.</div>
-                    </Content>
-                    <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
-                  </Layout>
-                </Layout>
-              )
-            )
-          ]
+          // [
+          //   (localStorage.getItem('isNewUser') ?
+          //     (
+          //       <WrappedDetailsForm/>
+          //     ): (
+          //         <Layout style={{ minHeight: '100vh' }}>
+          //         <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
+          //           <div className="logo" />
+          //           <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+          //             {this.state.collapsed ? (
+          //               <Menu.Item key="11" disabled style={{height:'50px'}}>
+          //                 <img
+          //                   alt="profile"
+          //                   style={{marginTop:'10px',marginLeft:'-12px', width:'40px', height: '40px'}}
+          //                   src={firebase.auth().currentUser.photoURL}
+          //                 />
+          //               </Menu.Item>
+          //             ) : (
+          //               <Menu.Item key="9" disabled style={{height:'75px'}}>
+          //                 <img
+          //                     alt="profile"
+          //                     style={{marginTop:'10px', width:'60px', height: '60px'}}
+          //                     src={firebase.auth().currentUser.photoURL}
+          //                   />&nbsp;&nbsp;
+          //                 <span style={{fontSize:'14px', color:'white', fontWeight:'bold'}}>{firebase.auth().currentUser.displayName}</span>
+          //               </Menu.Item>
+          //             )}
+          //             <Menu.Item key="1" style={{height:'60px', fontSize:'18px'}}>
+          //               <Icon type="stock" />
+          //               <span>Stocks</span>
+          //             </Menu.Item>
+          //             <Menu.Item key="5" style={{height:'60px'}}>
+          //               <Icon type="pie-chart" />
+          //               <span>Investments</span>
+          //             </Menu.Item>
+          //             <Menu.Item key="2" style={{height:'60px'}}>
+          //               <Icon type="desktop" />
+          //               <span>Customer Care</span>
+          //             </Menu.Item>
+          //             <Menu.Item key="3" style={{height:'60px'}}>
+          //               <Icon type="user" />
+          //               <span>User</span>
+          //             </Menu.Item>
+          //             <Menu.Item key="4" onClick={this.signOut} style={{height:'60px'}}>
+          //               <Icon type="file" />
+          //               <span>Logout</span>
+          //             </Menu.Item>
+          //           </Menu>
+          //         </Sider>
+          //         <Layout>
+          //           <Header style={{ background: '#fff', padding: 0 }} />
+          //           <Content style={{ margin: '0 16px' }}>
+          //             <Breadcrumb style={{ margin: '16px 0' }}>
+          //               <Breadcrumb.Item>User</Breadcrumb.Item>
+          //               <Breadcrumb.Item>Bill</Breadcrumb.Item>
+          //             </Breadcrumb>
+          //             <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>Bill is a cat.</div>
+          //           </Content>
+          //           <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+          //         </Layout>
+          //       </Layout>
+          //     )
+          //   )
+          // ]
+            // this.signOutNow()
+          window.location.href = 'http://localhost:8000/red-hat-pirates/dashboard'
+
         ) : (
           <div>
             <StyledFirebaseAuth
